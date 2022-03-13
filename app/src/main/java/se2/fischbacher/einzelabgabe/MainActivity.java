5279ded72e82a87c71c8f9b1f90537620352e5bc
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-03-13 17:58:25 +0100

    implement calculatePrime onClickListener

--- app/src/main/java/se2/fischbacher/einzelabgabe/MainActivity.java
+++ app/src/main/java/se2/fischbacher/einzelabgabe/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import se2.fischbacher.einzelabgabe.handler.impl.PrimeMatrikelNummerHandler;
 import se2.fischbacher.einzelabgabe.handler.impl.ServerMatrikelNummerHandler;
 import se2.fischbacher.einzelabgabe.threads.TCPThread;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView matrikelNummerInput;
     TextView matrikelNummerOutput;
     Button sendToServerButton;
+    Button calculatePrimNumbersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
         matrikelNummerInput = findViewById(R.id.MatrikelnummerEingabe);
         matrikelNummerOutput = findViewById(R.id.OutputTextField);
         sendToServerButton = findViewById(R.id.SendToServerButton);
+        calculatePrimNumbersButton = findViewById(R.id.CalculatePrimNumbersButton);
 
         sendToServerButton.setOnClickListener(view -> {
             ServerMatrikelNummerHandler mNummerHandler = new ServerMatrikelNummerHandler(this.matrikelNummerInput.getText().toString(), this.matrikelNummerOutput);
             TCPThread tcpThread = new TCPThread("se2-isys.aau.at", 53212, mNummerHandler);
             new Thread(tcpThread).start();
+        });
+
+        calculatePrimNumbersButton.setOnClickListener(view -> {
+            PrimeMatrikelNummerHandler primHandler = new PrimeMatrikelNummerHandler(this.matrikelNummerInput.getText().toString(), this.matrikelNummerOutput);
+            primHandler.calculateOutput();
+            primHandler.handleOutput();
         });
     }
 }
