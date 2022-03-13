47629c837721c74be65cd4df9cafcab0973de03c
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-03-13 17:57:58 +0100

    add primeHandler as calcHandler

--- app/src/main/java/se2/fischbacher/einzelabgabe/handler/impl/PrimeMatrikelNummerHandler.java
+++ app/src/main/java/se2/fischbacher/einzelabgabe/handler/impl/PrimeMatrikelNummerHandler.java
@@ -1,15 +1,33 @@
 package se2.fischbacher.einzelabgabe.handler.impl;
 
+import android.widget.TextView;
+
 import se2.fischbacher.einzelabgabe.handler.interfaces.CalculationHandler;
+import se2.fischbacher.einzelabgabe.helper.Prime;
 
 public class PrimeMatrikelNummerHandler implements CalculationHandler {
-    @Override
-    public String calculateOutput(String input) {
-        return null;
+    private String matrikelNummer;
+    private TextView matrikelNummerOutput;
+    private String allPrimeNumbers;
+
+    public PrimeMatrikelNummerHandler(String matrikelNummer, TextView output) {
+        this.matrikelNummer = matrikelNummer;
+        this.matrikelNummerOutput = output;
     }
 
     @Override
-    public void handleOutput(String output) {
+    public void calculateOutput() {
+        this.allPrimeNumbers = "";
+        for (int i = 0; i < this.matrikelNummer.length(); i++) {
+            Integer currentNumber = Integer.parseInt(String.valueOf(this.matrikelNummer.charAt(i)));
+            if(Prime.isPrime(currentNumber)) {
+                this.allPrimeNumbers += " " + currentNumber;
+            }
+        }
+    }
 
+    @Override
+    public void handleOutput() {
+        this.matrikelNummerOutput.setText(this.allPrimeNumbers);
     }
 }
--- app/src/main/java/se2/fischbacher/einzelabgabe/handler/interfaces/CalculationHandler.java
+++ app/src/main/java/se2/fischbacher/einzelabgabe/handler/interfaces/CalculationHandler.java
@@ -1,6 +1,6 @@
 package se2.fischbacher.einzelabgabe.handler.interfaces;
 
 public interface CalculationHandler {
-    String calculateOutput(String input);
-    void handleOutput(String output);
+    void calculateOutput();
+    void handleOutput();
 }
