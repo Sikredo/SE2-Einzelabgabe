633152413133e901260f92dea1044598437002eb
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-03-13 15:41:51 +0100

    implement tcpThread

--- app/src/main/java/se2/fischbacher/einzelabgabe/TCPThread.java
+++ app/src/main/java/se2/fischbacher/einzelabgabe/TCPThread.java
@@ -1,2 +1,36 @@
-package se2.fischbacher.einzelabgabe;public class TCPThread {
+package se2.fischbacher.einzelabgabe;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPThread implements Runnable {
+    private String host;
+    private Integer port;
+    private TCPHandler handler;
+
+    TCPThread(String host, Integer port, TCPHandler handler) {
+        this.host = host;
+        this.port = port;
+        this.handler = handler;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket(this.host, this.port);
+            DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            toServer.writeBytes(this.handler.getRequestString());
+            this.handler.handleServerResponse(fromServer.readLine());
+            socket.close();
+        } catch (IOException exception) {
+            Log.e(this.getClass().toString(), exception.getMessage());
+        }
+    }
 }
