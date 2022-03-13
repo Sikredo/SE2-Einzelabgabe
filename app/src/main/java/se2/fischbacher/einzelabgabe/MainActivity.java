a0cb843506f4d5138b038f79e020fce9d7574fa8
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-03-13 15:25:54 +0100

    initialize android objects

--- app/src/main/java/se2/fischbacher/einzelabgabe/MainActivity.java
+++ app/src/main/java/se2/fischbacher/einzelabgabe/MainActivity.java
@@ -3,12 +3,25 @@ package se2.fischbacher.einzelabgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView matrikelNummerInput;
+    TextView matrikelNummerOutput;
+    Button sendToServerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        matrikelNummerInput = findViewById(R.id.MatrikelnummerEingabe);
+        matrikelNummerOutput = findViewById(R.id.OutputTextField);
+        sendToServerButton = findViewById(R.id.SendToServerButton);
+
+        sendToServerButton.setOnClickListener(view -> {
+
+        });
     }
 }
